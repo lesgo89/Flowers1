f915e67a0e17fa3f3bbc830fa4e3a54275faa363
lesgo89 <33404599+lesgo89@users.noreply.github.com>
2024-09-21 13:33:33 -0500

    Add files via upload

--- anim.js
+++ anim.js
@@ -4,35 +4,23 @@ var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "At the time", time: 15 },
-  { text: "The whisper of birds", time: 18 },
-  { text: "Lonely before the sun cried", time: 27 },
-  { text: "Fell from the sky", time: 32 },
-  { text: "Like water drops", time: 33 },
-  { text: "Where I'm now? I don't know why", time: 41 },
-  { text: "Nice butterflies in my hands", time: 47 },
-  { text: "Too much light for twilight", time: 54 },
-  { text: "In the mood for the flowers love", time: 59 },
-  { text: "That vision", time: 67 },
-  { text: "Really strong, blew my mind", time: 72 },
-  { text: "Silence Let me see what it was", time: 78 },
-  { text: "I only want to live in clouds", time: 83 },
-  { text: "Where I'm now? I don't know why", time: 91 },
-  { text: "Nice butterflies in my hands", time: 97 },
-  { text: "Too much light for twilight", time: 104 },
-  { text: "In the mood for the flowers love", time: 108 },
-  { text: "At the time", time: 144 },
-  { text: "The whisper of birds", time: 148 },
-  { text: "Lonely before the sun cried", time: 153 },
-  { text: "Fell from the sky", time: 158 },
-  { text: "Like water drops", time: 164 },
-  { text: "Where I'm now? I don't know why", time: 169 },
-  { text: "Nice butterflies in my hands", time: 176 },
-  { text: "Too much light for twilight", time: 183 },
-  { text: "In the mood for the flowers", time: 188 },
-  { text: "Love.", time: 140 },
+  { text: "Como las flores que tanto te gustan,  ", time: 15 },
+  { text: "mi amor por ti ilumina cada día", time: 18 },
+  { text: "aunque no estés aquí", time: 27 },
+  { text: "Te extraño profundamente", time: 32 },
+  { text: "esperando el momento en que volvamos a florecer juntos", time: 33 },
+  { text: "Como en la canción Yellow", time: 41 },
+  { text: "mi amor por ti sigue brillando.", time: 47 },
+  { text: "Te extraño y espero con ansias", time: 54 },
+  { text: "el día en que estemos juntos de nuevo", time: 59 },
+  { text: "Mi amor Nadia.", time: 67 },
+
 ];
 
+//Como las flores que tanto te gustan, mi amor por ti ilumina cada día, aunque no estés aquí. 
+  //    Te extraño profundamente, esperando el momento en que volvamos a florecer juntos
+//"Como en la canción Yellow, mi amor por ti sigue brillando. Te extraño y espero con ansias el día en que estemos juntos de nuevo, Nadia.
+//Atte; Jean C."
 // Animar las letras
 function updateLyrics() {
   var time = Math.floor(audio.currentTime);
